e03f30ca0700f720265d0f1f410d1e5d8888eb37
mamane19 <b.moussaama@alustudent.com>
2021-07-08 18:07:29 +0200

    ✂️ : Connect API

--- script.js
+++ script.js
@@ -1,9 +1,101 @@
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
-});
+baseUrl = "http://localhost:3001";
+// $(function () {
+//   $(window).on("scroll", function () {
+//     if ($(window).scrollTop() > 10) {
+//       $(".navbar").addClass("active");
+//     } else {
+//       $(".navbar").removeClass("active");
+//     }
+//   });
+// });
+
+// SignIn / LogIn
+function onLoginDetailsSubmit() {
+  var formData = {};
+  formData["email_address"] = document.getElementById("email_address").value;
+  formData["password"] = document.getElementById("password").value;
+
+  userLogin(formData);
+}
+function userLogin(data) {
+  var postData = JSON.stringify(data);
+  $.ajax({
+    type: "POST",
+    url: baseUrl + "/users/signin",
+    dataType: "json",
+    data: postData,
+    contentType: "application/json; charset=utf-8",
+    cache: false,
+    success: function (response) {
+      var data = response.user;
+      console.log(data);
+
+      console.log("token:" + response.token);
+
+    //   document.cookie = "authToken=" + response.token;
+      console.log("hey there")
+    //   document.cookie = "authToken=" + response.token;
+    //   window.location.href = "./home.html";
+    },
+    headers: {
+      Accept: "application/json; charset=utf-8",
+      Content_Type: "application/json; charset=utf-8",
+      Authorization: getCookie("authToken"),
+    },
+  });
+}
+
+function getCookie(name) {
+  // Split cookie string and get all individual name=value pairs in an array
+  var cookieArr = document.cookie.split(";");
+
+  // Loop through the array elements
+  for (var i = 0; i < cookieArr.length; i++) {
+    var cookiePair = cookieArr[i].split("=");
+
+    /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+    if (name == cookiePair[0].trim()) {
+      // Decode the cookie valu
+      console.log("cookie:" + decodeURIComponent(cookiePair[1]));
+      return decodeURIComponent(cookiePair[1]);
+    }
+  }
+
+  // Return null if not found
+  return null;
+}
+
+// SignUp
+function onCreateUser() {
+  var formData = {};
+  formData["email_address"] = document.getElementById("email-create").value;
+  formData["password"] = document.getElementById("password-create").value;
+  formData["user_name"] = document.getElementById("name-create").value;
+  addUser(formData);
+  // window.location.href = "./index.html";
+//   clearUserForm();
+}
+function addUser(data) {
+  var postData = JSON.stringify(data);
+  $.ajax({
+    type: "POST",
+    url: baseUrl + "/users/signup",
+    dataType: "json",
+    data: postData,
+    contentType: "application/json; charset=utf-8",
+    cache: false,
+    success: function (response) {
+      var data = response.data;
+      console.log(data);
+    //   addUserRecordToTable(data);
+      window.location.href = "./home.html";
+    },
+
+    headers: {
+      Accept: "application/json; charset=utf-8",
+      Content_Type: "application/json; charset=utf-8",
+      Authorization: getCookie("authToken"),
+    },
+  });
+}
